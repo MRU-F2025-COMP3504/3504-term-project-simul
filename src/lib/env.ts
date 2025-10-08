6f0013b212d3cdd3cffe55b0caa1643ed247c916
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-10-07 20:58:35 -0600

    fix: implemented server/client env handling

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,21 +1,21 @@
+import { loadEnv } from "@matthew-hre/env";
 import { z } from "zod";
 
-const envSchema = z.object({
-    DATABASE_URL: z.string().min(1, {message: "DATABASE_URL is required."}),
+const envSchema = {
+  server: z.object({
+    DATABASE_URL: z.url({
+      protocol: /^postgres(ql)?:/,
+    }).min(1, { message: "DATABASE_URL is required." }),
     NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
     POSTGRES_PORT: z.coerce.number().default(5432),
-    
-    //not sure how to make the ones for password and user
-    POSTGRES_USER: z.string().min(1, {message: "POSTGRES_USER is required"}),
-    POSTGRES_PASSWORD: z.string().min(1, {message: "POSTGRES_PASSWORD is required"}),
 
-});
+    POSTGRES_USER: z.string().min(1, { message: "POSTGRES_USER is required" }),
+    POSTGRES_PASSWORD: z.string().min(1, { message: "POSTGRES_PASSWORD is required" }),
+  }),
+  client: z.object({}),
+};
 
-const parsedEnv = envSchema.safeParse(process.env);
+export type ServerEnvSchema = z.infer<typeof envSchema.server>;
+export type ClientEnvSchema = z.infer<typeof envSchema.client>;
 
-if (!parsedEnv.success) {
-    console.error("Invalid environment variables:", parsedEnv.error.format());
-    throw new Error("Invalid environment variables");
-}
-
-export const env = parsedEnv.data;
+export const { serverEnv, clientEnv } = loadEnv(envSchema);
