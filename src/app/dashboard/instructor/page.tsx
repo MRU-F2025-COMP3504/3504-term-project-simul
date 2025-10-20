2e11a8f1ec2add1b709528fb98f558914ef4adea
Sunny <113143064+aparm539@users.noreply.github.com>
2025-10-20 13:13:51 -0600

    save wip

--- src/app/dashboard/instructor/page.tsx
+++ src/app/dashboard/instructor/page.tsx
@@ -12,6 +12,7 @@ export default function CodeEditor() {
   const [history, setHistory] = useState<Transaction[]>([]);
   const [recording, setRecording] = useState(false);
   const [playbackTime, setPlaybackTime] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
   function recordChanges(tr: Transaction) {
     if (recording) {
       if ((tr.annotation(Transaction.userEvent))) {
@@ -37,39 +38,48 @@ export default function CodeEditor() {
 
     },
   });
-  const handleclick = async () => {
-    if (!view)
-      return;
-    // Match the state from the beginning of the recording
-    view.setState(history[0].startState);
 
-    for (let i = 0; i < history.length; i++) {
-      const event: Transaction = history[i];
-      const changes: ChangeSet = event.changes;
-      if (changes) {
+  // Create a wrapping type for transactions. Store time and the transaction itself.
+  const handlePlayback: () => Promise<void> = async () => {
+    // eslint-disable-next-line no-unmodified-loop-condition
+    while (isPlaying) {
+      if (!view)
+        return;
+      // Match the state from the beginning of the recording
+      view.setState(history[0].startState);
+
+      for (let i = 0; i < history.length; i++) {
+        const event: Transaction = history[i];
+        const changes: ChangeSet = event.changes;
+        if (changes) {
         // We must build a new transaction to apply the changes
         // When I didn't do this and just dispatched the old transaction,
         // The editor would not update correctly.
         // Could just be a skill issue.
-        const tr: Transaction = view.state.update({ changes });
-        view.dispatch(tr);
-      }
+          const tr: Transaction = view.state.update({ changes });
+          view.dispatch(tr);
+        }
 
-      // Check if the event is the last one
-      if (i === history.length - 1) {
-        break;
-      }
-      const nextEvent: Transaction = history[i + 1];
-      // Guard me
-      if (!nextEvent) {
-        break;
-      }
-      // Calculate me
-      const delay = (nextEvent?.annotation(Transaction?.time) ?? 0) - (event?.annotation(Transaction.time) ?? 0);
+        // Check if the event is the last one
+        if (i === history.length - 1) {
+          break;
+        }
+        const nextEvent: Transaction = history[i + 1];
+        // Guard me
+        if (!nextEvent) {
+          break;
+        }
+        // Calculate me
+        const delay = (nextEvent?.annotation(Transaction?.time) ?? 0) - (event?.annotation(Transaction.time) ?? 0);
 
-      // Delay me
-      await new Promise(resolve => setTimeout(resolve, delay));
+        // Delay me
+        await new Promise(resolve => setTimeout(resolve, delay));
+      }
     }
+  };
+  const togglePlayback = () => {
+    setIsPlaying(!isPlaying);
+    handlePlayback();
   };
 
   const calculatePlaybackTime = () => {
@@ -82,14 +92,44 @@ export default function CodeEditor() {
     const playbackTime = (lastEvent.annotation(Transaction.time) ?? 0) - (firstEvent.annotation(Transaction.time) ?? 0);
     setPlaybackTime(playbackTime);
   };
-  const toggleRecording = async () => {
-    setRecording(!recording);
-    if (recording === true) {
-      setHistory([]);
+
+  const formatDisplayTime = (timeInMs: number) => {
+    const parsedSeconds = Math.floor((timeInMs / 1000) % 60);
+    const parsedMinutes = Math.floor((timeInMs / (1000 * 60)) % 60);
+    const parsedHours = Math.floor((timeInMs / (1000 * 60 * 60)) % 24);
+    let formattedSeconds = "";
+    let formattedMinutes = "";
+    let formattedHours = "";
+
+    if (parsedSeconds < 10) {
+      formattedSeconds = `0${parsedSeconds}`;
     }
     else {
-      calculatePlaybackTime();
+      formattedSeconds = `${parsedSeconds}`;
     }
+    if (parsedMinutes < 10) {
+      formattedMinutes = `0${parsedMinutes}`;
+    }
+    else {
+      formattedMinutes = `${parsedMinutes}`;
+    }
+    if (parsedHours < 10) {
+      formattedHours = `0${parsedHours}`;
+    }
+    else {
+      formattedHours = `${parsedHours}`;
+    }
+
+    if (parsedHours === 0) {
+      return `${formattedMinutes}:${formattedSeconds}`;
+    }
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  const toggleRecording = async () => {
+    setRecording(!recording);
+    calculatePlaybackTime();
   };
 
   useEffect(() => {
@@ -103,10 +143,10 @@ export default function CodeEditor() {
       <Button onClick={toggleRecording}>
         {recording ? "Stop Recording" : "Start Recording"}
       </Button>
-      <Button onClick={handleclick}> play </Button>
+      <Button onClick={togglePlayback}> play </Button>
       <div>
         Playback time:
-        {playbackTime}
+        {formatDisplayTime(playbackTime)}
       </div>
       <div ref={editor} />
 
