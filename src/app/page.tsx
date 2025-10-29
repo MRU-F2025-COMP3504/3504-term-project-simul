70b65e5fd32a8aa4f99da06507a27834ecfbdfb4
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-10-29 16:25:34 -0600

    fix: added signin button to `/`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import SignInButton from "~/components/auth/signin-button";
 import { ThemeToggle } from "~/components/theme-toggle";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col p-8 pb-20 font-sans">
       <ThemeToggle />
+      <SignInButton />
     </div>
   );
 }
